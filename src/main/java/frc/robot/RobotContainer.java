3d7151e2ff0d201d9a3cfac95eab8de5dca74eb5
SamH434 <77312898+SamH434@users.noreply.github.com>
2023-08-27 11:49:08 -0400

    Fixed robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.OIConstants.LogitechControllerDevice;
+import frc.robot.Constants.OIConstants.ControllerDevice;
 import frc.robot.Devices.Controller;
 import frc.robot.commands.Autos;
 import frc.robot.commands.DriveManuallyCommand;
@@ -87,8 +87,8 @@ public class RobotContainer {
        * to the
        * commands that need manual control input (e.g. DriveManuallyCommand)
        */
-      driveStick = new Controller(LogitechControllerDevice.DRIVESTICK);
-      turnStick = new Controller(LogitechControllerDevice.TURNSTICK);
+      driveStick = new Controller(ControllerDevice.DRIVESTICK);
+      turnStick = new Controller(ControllerDevice.TURNSTICK);
       System.out.println("Driver interface configured");
   }
 
